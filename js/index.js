985e57d21213eb182a85214e19335b90ab59c798
LizakKhona <liz.honahbeeva2009@gmail.com>
2025-06-19 21:37:46 +0200

    loop

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i += 1;
+// }
+
+
+// for (let k = 1; k <= 20; k += 1){
+//     if (k % 2 !== 0) {
+//         continue
+//     } else {
+//         console.log(k)
+//     }
+// }
+
+
+// for (let number = 1; number <= 10; number += 1){
+//     let result = 7 * number;
+//     console.log(result)
+// }
+
+
+//
+
+let n = 5;
+for (let i = 0; i <= 5; i++) {
+    if (i === n) {
+        break
+    }
+    console.log(i)
+}
+
+// let figure = 1;
+// while (figure <= 20) {
+//     if (figure % 3 === 0) {
+//         figure = figure + 1;
+//         continue
+//     } else {
+//         console.log(figure);
+//     }
+//     figure = figure + 1;
+// }
